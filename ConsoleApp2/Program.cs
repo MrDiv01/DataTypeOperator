edd05adbd985aee33e5c09f862d28f6d14f3da3e
Nurlan Məmmədov <nurlan.memmedov8818@gmail.com>
2023-08-29 20:42:49 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Task1
+//Console.WriteLine("Salam Dunya");
+//Task2
+//int num1 = 1;
+//Console.WriteLine(num1);
+//Task4
+//string num1 = Console.ReadLine();
+//string num2 = Console.ReadLine();
+//int number1 = 0;
+//int number2 = 0;
+//bool checkNum1 = int.TryParse(num1, out number1);
+//bool checkNum2 = int.TryParse(num2, out number2);
+//if (checkNum1 == true && checkNum2 == true)
+//{
+//    int result = number1 * number2;
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//    Console.WriteLine("Reqemden Basqa sinvol daxil etmek olmaz");
+//}
+//Task5
+//Console.WriteLine("Adinizi Daxil edin.");
+//string name = Console.ReadLine();
+//Console.WriteLine("Soyadiniz Daxil edin.");
+//string surename = Console.ReadLine();
+//Console.WriteLine("Tevelludunuzu Daxil edin.");
+//string date = Console.ReadLine();
+//Console.WriteLine("Menim adim "+name  +" Menim soyadim  "+surename +" Menim Tevelludum "+date);
